2565df71e16f03bf124b8a2ee2017f766f598232
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-05-31 21:10:37 +0600

    Product model and schema created successfully.

---
+++ src/app/modules/products/products.model.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose";
+import { IProducts } from "./products.interface";
+
+const productSchema = new Schema<IProducts>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: { type: [{ type: String, value: String }], required: true },
+  inventory: { type: { quantity: Number, inStock: Boolean }, required: true },
+});
+
+const Product = mongoose.model<IProducts>("Product", productSchema);
+
+export default Product;
