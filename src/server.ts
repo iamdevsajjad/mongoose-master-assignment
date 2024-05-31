d6dc9c0fb6b6b4bb784ab3a3471b74d22d1d7885
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-05-31 20:26:53 +0600

    mongoose setup done .

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
+import mongoose from "mongoose";
 import app from "./app";
+import config from "./app/config";
 
 const port = 5000;
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+
+async function main() {
+  try {
+    await mongoose.connect(config.dbUrl as string);
+
+    app.listen(config.port || port, () => {
+      console.log(`server is running on port ${config.port || port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
