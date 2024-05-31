2bd3f77a3f6984c82d63ce9cf65af4a1ad271157
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-05-31 20:52:37 +0600

    product type declared.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
+import cors from "cors";
 import express, { Request, Response } from "express";
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello this is Mongoose master");
 });
+
 export default app;
